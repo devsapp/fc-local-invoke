57215acc89a8633c2ab0f1cc363d7217b9289f80
千风 <qianfeng.zqf@alibaba-inc.com>
2021-07-30 22:04:37 +0800

    fix: adjust localRoot in vscode debug mode
    change localRoot from build path to src code path when in vscode debug mode

--- src/lib/interface/fc-function.ts
+++ src/lib/interface/fc-function.ts
@@ -16,6 +16,7 @@ export interface FunctionConfig {
   instanceConcurrency?: number;
   instanceType?: string;
   codeUri?: string;
+  originalCodeUri?: string;
 }
 
 export interface CustomContainerConfig {
