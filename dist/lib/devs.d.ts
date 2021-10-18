0ad6581f6d6b266a5b8c107316b9e3663933edc1
wss-git <wssgryx@163.com>
2021-10-18 17:35:03 +0800

    build: 0.0.21

--- dist/lib/devs.d.ts
+++ dist/lib/devs.d.ts
@@ -4,4 +4,11 @@ export declare const DEFAULT_NAS_PATH_SUFFIX: string;
 export declare function getRootBaseDir(baseDir: string): string;
 export declare function detectNasBaseDir(devsPath: string): string;
 export declare function detectTmpDir(devsPath: string, tmpDir?: string): string;
-export declare function updateCodeUriWithBuildPath(baseDir: string, functionConfig: FunctionConfig, serviceName: string): FunctionConfig;
+export declare function updateCodeUriWithBuildPath(baseDir: string, functionConfig: FunctionConfig, serviceName: string): Promise<FunctionConfig>;
+/**
+ * 检测 build 是否可用
+ * @param serviceName 服务名称
+ * @param functionName 函数名称
+ */
+export declare function checkBuildAvailable(baseDir: any, serviceName: string, functionName: string): Promise<void>;
+export declare function isInterpretedLanguage(runtime: string): boolean;
--- dist/lib/invoke/http-invoke.d.ts
+++ dist/lib/invoke/http-invoke.d.ts
@@ -9,6 +9,7 @@ export default class HttpInvoke extends Invoke {
     private _invokeInitializer;
     private runner;
     private watcher?;
+    private limitedHostConfig?;
     constructor(creds: ICredentials, region: string, baseDir: string, serviceConfig: ServiceConfig, functionConfig: FunctionConfig, triggerConfig?: TriggerConfig, debugPort?: number, debugIde?: any, tmpDir?: string, authType?: string, endpointPrefix?: string, debuggerPath?: any, debugArgs?: any, nasBaseDir?: string);
     _disableRunner(evt: any, name: any): void;
     beforeInvoke(): Promise<void>;
